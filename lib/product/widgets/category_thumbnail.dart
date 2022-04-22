6ede9d3ebc9edd382c84139dc13c55c3494c46b2
abubakarshaikh <atifa2019@gmail.com>
2022-04-22 21:27:50 +0500

    update

--- lib/product/widgets/category_thumbnail.dart
+++ lib/product/widgets/category_thumbnail.dart
@@ -1,182 +1,205 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:towermarket/colors/colors.dart';
-import 'package:towermarket/models/category.dart';
 import 'package:towermarket/product/bloc/category_bloc.dart';
-import 'package:towermarket/typography/text_styles.dart';
+import '../../models/category.dart';
+import '../../typography/text_styles.dart';
 
-class CategoryThumbnail extends StatelessWidget {
+class CategoryThumbnail extends StatefulWidget {
   final Category category;
-  const CategoryThumbnail({
-    Key? key,
-    required this.category,
-  }) : super(key: key);
+
+  const CategoryThumbnail({Key? key, required this.category}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        AspectRatio(
-          aspectRatio: 1 / 1,
-          child: Column(
-            children: [
-              category.isSelected
-                  ? _SelectedThumbnail(category: category)
-                  : _UnSelectedThumbnail(category: category),
-              Container(
-                height: 12,
-                color: Colors.white,
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
+  State<CategoryThumbnail> createState() => _CategoryThumbnailState();
 }
 
-class _UnSelectedThumbnail extends StatelessWidget {
-  const _UnSelectedThumbnail({
-    Key? key,
-    required this.category,
-  }) : super(key: key);
+class _CategoryThumbnailState extends State<CategoryThumbnail> {
+  double _bottom = -24;
+  double _top = 14;
 
-  final Category category;
+  _updateState() {
+    setState(() {
+      _bottom = -12;
+      _top = 0;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: GestureDetector(
-        onTap: () {
-          context.read<CategoryBloc>().add(
-              CategorySelect(category: category));
-        },
-        child: Row(
-          children: [
-            Expanded(
-              child: Column(
-                children: [
-                  Expanded(
-                    flex: 2,
-                    child: Container(
-                      margin: const EdgeInsets.only(left: 12, right: 12),
-                      decoration: BoxDecoration(
-                        color: TowerMarketColors.lightgrey,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8),
+      child: AspectRatio(
+        aspectRatio: 1 / 1,
+        child: GestureDetector(
+          onTap: () {
+            context
+                .read<CategoryBloc>()
+                .add(CategorySelect(category: widget.category));
+            _updateState();
+          },
+          child: Row(
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    Expanded(
+                      flex: 2,
                       child: Stack(
-                        clipBehavior: Clip.none,
                         alignment: Alignment.center,
                         children: [
-                          Positioned(
-                            top: 8, // 8
-                            bottom: -24, //-12
-                            child: Image.asset(category.image),
+                          AnimatedPositioned(
+                            duration: const Duration(milliseconds: 200),
+                            right: 12,
+                            left: 12,
+                            top: 6,
+                            bottom: 0,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: widget.category.isSelected
+                                    ? TowerMarketColors.lightyellow
+                                    : TowerMarketColors.lightgrey,
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+                          ),
+                          AnimatedPositioned(
+                            duration: const Duration(milliseconds: 500),
+                            top: _top,
+                            left: 12,
+                            right: 12,
+                            bottom: _bottom,
+                            child: Container(
+                              alignment: Alignment.bottomCenter,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              child: Image.asset(widget.category.image),
+                            ),
                           ),
                         ],
                       ),
                     ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      color: Colors.white,
-                      child: Text(
-                        category.title,
-                        style: TowerMarketTextStyle.title3,
-                        textAlign: TextAlign.center,
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
+                    Expanded(
+                      child: Container(
+                        alignment: Alignment.bottomCenter,
+                        child: Text(
+                          widget.category.title,
+                          style: TowerMarketTextStyle.title3.copyWith(
+                            color: TowerMarketColors.grey,
+                          ),
+                          textAlign: TextAlign.center,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              width: 3.5,
-              height: double.infinity,
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(4),
-                  bottomLeft: Radius.circular(4),
+                  ],
                 ),
               ),
-            ),
-          ],
+              Container(
+                width: 3.5,
+                height: double.infinity,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(4),
+                    bottomLeft: Radius.circular(4),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-class _SelectedThumbnail extends StatelessWidget {
-  const _SelectedThumbnail({
-    Key? key,
-    required this.category,
-  }) : super(key: key);
+// class _SelectedThumbnail extends StatelessWidget {
+//   const _SelectedThumbnail({
+//     Key? key,
+//     required this.category,
+//   }) : super(key: key);
 
-  final Category category;
+//   final Category category;
 
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: Row(
-        children: [
-          Expanded(
-            child: Column(
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: Container(
-                    margin: const EdgeInsets.only(left: 12, right: 12),
-                    decoration: BoxDecoration(
-                      color: TowerMarketColors.lightyellow,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Stack(
-                      clipBehavior: Clip.none,
-                      alignment: Alignment.center,
-                      children: [
-                        Positioned(
-                          top: -8,
-                          bottom: -12,
-                          child: Image.asset(category.image),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    color: Colors.white,
-                    child: Text(
-                      category.title,
-                      style: TowerMarketTextStyle.title3.copyWith(
-                        fontWeight: FontWeight.w600,
-                      ),
-                      textAlign: TextAlign.center,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            width: 3.5,
-            height: double.infinity,
-            decoration: const BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(4),
-                bottomLeft: Radius.circular(4),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return GestureDetector(
+//       onTap: () {
+//         context.read<CategoryBloc>().add(CategorySelect(category: category));
+//       },
+//       child: Row(
+//         children: [
+//           Expanded(
+//             child: Column(
+//               children: [
+//                 Expanded(
+//                   flex: 2,
+//                   child: Stack(
+//                     alignment: Alignment.center,
+//                     children: [
+//                       Positioned(
+//                         right: 12,
+//                         left: 12,
+//                         top: 6,
+//                         bottom: 0,
+//                         child: Container(
+//                           decoration: BoxDecoration(
+//                             color: TowerMarketColors.lightyellow,
+//                             borderRadius: BorderRadius.circular(12),
+//                           ),
+//                         ),
+//                       ),
+//                       Positioned(
+//                         right: 8,
+//                         left: 8,
+//                         top: 0,
+//                         bottom: -12,
+//                         child: Container(
+//                           alignment: Alignment.bottomCenter,
+//                           decoration: BoxDecoration(
+//                             borderRadius: BorderRadius.circular(12),
+//                           ),
+//                           child: Hero(
+//                               tag: 'Category',
+//                               child: Image.asset(category.image)),
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//                 Expanded(
+//                   child: Container(
+//                     alignment: Alignment.bottomCenter,
+//                     child: Text(
+//                       category.title,
+//                       style: TowerMarketTextStyle.title3.copyWith(
+//                         color: TowerMarketColors.black,
+//                         fontWeight: TowerMarketFontWeight.bold,
+//                       ),
+//                       textAlign: TextAlign.center,
+//                       maxLines: 1,
+//                       overflow: TextOverflow.ellipsis,
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//           Container(
+//             width: 4,
+//             height: double.infinity,
+//             decoration: const BoxDecoration(
+//               color: Colors.black,
+//               borderRadius: BorderRadius.only(
+//                 topLeft: Radius.circular(4),
+//                 bottomLeft: Radius.circular(4),
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
